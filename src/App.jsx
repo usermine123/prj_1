35b8c2cfae6cc02a4b30e74d5b184123ba8ee04f
unknown <daneshh08@gmial.com>
2023-06-16 18:32:24 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Header from './Header.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div >
+  )
+}
+export default App
---
+++ src/Header.js
@@ -0,0 +1,6 @@
+const Header = () => {
+    return (
+        <h1>groceries list</h1>
+    )
+}
+export default Header
